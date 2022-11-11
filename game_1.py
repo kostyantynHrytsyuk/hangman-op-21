762480dd0fd5c80fc67388137efea18f18e53bef
PavloKukurik <116553132+PavloKukurik@users.noreply.github.com>
2022-11-11 11:36:52 +0200

    1

--- game_1.py
+++ game_1.py
@@ -13,10 +13,12 @@ def check_print_words(word):
         letter=input('Please guess a letter: ')
         if_letter_in_alphabet= check(letter, word, all_letters)
         if if_letter_in_alphabet==True:
+            number_of_attempts -= 1
+            print(f'Good guess: {new_str}')
         elif if_letter_in_alphabet==False:
             number_of_attempts-=1
             print(f'Oops! That letter is not in my word: {new_str}')
-        elif if_letter_in_alphabet=='Oops':
+        elif if_letter_in_alphabet =='Oops':
             number_of_attempts-=1
             print(f'Oops! You*ve already guessed that letter: {new_str}')
         else:
