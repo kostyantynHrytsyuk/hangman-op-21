8953debd338bc8f999e024c532ac5f6a4c5c5f19
VikaProkhorova <prokhorova.pn@ucu.edu.ua>
2022-11-11 09:47:36 +0200

    print

--- game_1.py
+++ game_1.py
@@ -1,4 +1,10 @@
-def game(word, letter):
+def check_print_words(word, letter):
     """
     number_of_attempts, word -> str , number_of_letter->
     """
+    all_letters='abcdefghijklmnopqrstuvwxyz'
+    number_of_attempts= 8
+    for i in range(len(word)):
+        print(f'You have {number_of_attempts} guesses left')
+        print(f'Available letters: {all_letters}')
+        letter=input('Please guess a letter: ')
