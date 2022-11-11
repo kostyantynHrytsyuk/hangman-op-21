16d8df08a2e1bfb39c5461b98732ad5d2098f864
niuta24 <syvkova.pn@ucu.edu.ua>
2022-11-11 09:24:49 +0300

    Adding branch read_file with file hangman.py

---
+++ hangman.py
@@ -0,0 +1,20 @@
+import random
+def load_words(filename:str)->str:
+    """
+    Args:
+        filename(str): path to file
+    returns random word from the file 
+    >>> load_words(9)
+    """
+    if isinstance(filename, str) is True:
+        with open(filename, 'r', encoding='utf-8') as file:
+            all_words=file.readlines()
+        massive=all_words[0].split(' ')
+        result=random.randint(0, len(massive))
+        print(f"""
+Loading word list from file...
+{len(massive)} words loaded.
+Welcome to the game, Hangman!
+I am thinking of a word that is {len(massive[result])} letters long.
+    """)
+        return massive[result]
