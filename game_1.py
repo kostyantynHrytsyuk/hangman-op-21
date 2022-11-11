e63bacc627be6069f2bbe98f205e8da2fb78f941
VikaProkhorova <prokhorova.pn@ucu.edu.ua>
2022-11-11 12:43:44 +0200

    done

--- game_1.py
+++ game_1.py
@@ -4,37 +4,33 @@ def check_print_words(word):
     """
     all_letters='abcdefghijklmnopqrstuvwxyz'
     number_of_attempts= 8
-    new_str=''
+    new_lst=[]
+    change= len(word)
     for j in range(len(word)):
-        new_str+='_'
-    for i in range(len(word)):   
+        new_lst.append('_')
+    while number_of_attempts>0 and change>0:
         print(f'You have {number_of_attempts} guesses left')
         print(f'Available letters: {all_letters}')
         letter=input('Please guess a letter: ')
         if_letter_in_alphabet= check(letter, word, all_letters)
-<<<<<<< HEAD
-        if if_letter_in_alphabet==True:
-            number_of_attempts -= 1
-            print(f'Good guess: {new_str}')
+        if if_letter_in_alphabet==True:    
+            for k in range(len(word)):
+                if word[k]==letter:
+                    change-=1
+                    new_lst.pop(k)
+                    new_lst.insert(k, letter)
+            new_new_lst=''.join(new_lst)
+            print(f'Good guess: {new_new_lst}')
         elif if_letter_in_alphabet==False:
             number_of_attempts-=1
-            print(f'Oops! That letter is not in my word: {new_str}')
+            new_new_lst=''.join(new_lst)
+            print(f'Oops! That letter is not in my word: {new_new_lst}')
         elif if_letter_in_alphabet =='Oops':
             number_of_attempts-=1
-            print(f'Oops! You*ve already guessed that letter: {new_str}')
+            new_new_lst=''.join(new_lst)
+            print(f'Oops! You*ve already guessed that letter: {new_new_lst}')
         else:
             number_of_attempts-=1
-            print(f'Oops! This is not a letter: {new_str}')
-=======
-        if if_letter_in_alphabet==True:    
-            word_SP=word.split('')
-            for o,el in word_SP:
-                if el==letter:
-                    new_str.replace(new_str[o],letter)
-            print(f'Good guess: {new_str}')
-        elif if_letter_in_alphabet ==False:
-            print('Oops! ThaT letter is not in my word: ')   
-        else:
-            number_of_attempts-=1
-            print(f('Oops!'))
->>>>>>> dc4a1f7 (if letter=el)
+            new_new_lst=''.join(new_lst)
+            print(f'Oops! This is not a letter: {new_new_lst}')
+        all_letters=all_letters.replace(letter, '')
