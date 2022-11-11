72666badecf6fb7d8d441e0d3d418e91f41cc97f
vitaliypn <paliichu.pn@ucu.edu.ua>
2022-11-11 10:03:27 +0200

    without doctest

--- hangman.py
+++ hangman.py
@@ -1,6 +1,5 @@
 """Reading file"""
 import random
-import doctest
 
 def load_words(filename:str)->str:
     """
@@ -9,18 +8,17 @@ def load_words(filename:str)->str:
     returns random word from the file 
     >>> load_words(9)
     """
-    if isinstance(filename, str) is True:
-        with open(filename, 'r', encoding='utf-8') as file:
-            all_words=file.readlines()
-        massive=all_words[0].split(' ')
-        result=random.randint(0, len(massive))
-        print(f"""
+    if not isinstance(filename, str):
+        return ''
+    with open(filename, 'r', encoding='utf-8') as file:
+        all_words=file.readlines()
+    massive=all_words[0].split(' ')
+    result=random.randint(0, len(massive))
+    print(f"""
 Loading word list from file...
 {len(massive)} words loaded.
 Welcome to the game, Hangman!
 I am thinking of a word that is {len(massive[result])} letters long.
-    """)
-        return massive[result]
+""")
+    return massive[result]
 
-if __name__=="__main__":
-    doctest.testmod()
