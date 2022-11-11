3eb064f30e209615577d01bd9de6ad66a1bc2fb6
yarkapetruniv <petruniv.pn@ucu.edu.ua>
2022-11-11 11:00:02 +0200

    3 function

--- hangman.py
+++ hangman.py
@@ -1,2 +1,22 @@
-def check(word):
-    
+def check(letter, word, alphabet):
+    """
+    Check whether the given letter is string and is in alphabet.
+    If the given letter is not a string the function should return None.
+    If letter not in alphabet, it means that player has already
+    chosen that letter and the function should return "Oops". If the given
+    letter is in guessed word the function should return True. Else the
+    function should return False.
+    """
+    if len(letter) == 1 and isinstance(letter, str):
+            if letter not in alphabet:
+                return "Oops"
+            else:
+                if letter in word:
+                    return True
+                return False
+    return None
+
+
+
+
+ 
