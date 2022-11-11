421e99b0582a3091ee77cdd29801a493d974cc67
niuta24 <syvkova.pn@ucu.edu.ua>
2022-11-11 09:52:50 +0300

    changing mistakes

--- hangman.py
+++ hangman.py
@@ -1,4 +1,5 @@
 import random
+
 def load_words(filename:str)->str:
     """
     Args:
@@ -6,15 +7,16 @@ def load_words(filename:str)->str:
     returns random word from the file 
     >>> load_words(9)
     """
-    if isinstance(filename, str) is True:
-        with open(filename, 'r', encoding='utf-8') as file:
-            all_words=file.readlines()
-        massive=all_words[0].split(' ')
-        result=random.randint(0, len(massive))
-        print(f"""
+    if not isinstance(filename, str):
+        return ''
+    with open(filename, 'r', encoding='utf-8') as file:
+        all_words=file.readlines()
+    massive=all_words[0].split(' ')
+    result=random.randint(0, len(massive))
+    print(f"""
 Loading word list from file...
 {len(massive)} words loaded.
 Welcome to the game, Hangman!
 I am thinking of a word that is {len(massive[result])} letters long.
-    """)
-        return massive[result]
+""")
+    return massive[result]
