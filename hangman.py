03c84c5c3fc8296c90f8cd2919f2a455b0e01cb8
vitaliypn <paliichu.pn@ucu.edu.ua>
2022-11-14 14:38:06 +0200

    chabged

--- hangman.py
+++ hangman.py
@@ -1,6 +1,36 @@
 """Reading file"""
 import random
 
+
+def check(letter, word, alphabet):
+    """
+    Check whether the given letter is string and is in alphabet.
+    If the given letter is not a string the function should return None.
+    If letter not in alphabet, it means that player has already
+    chosen that letter and the function should return "Oops". If the given
+    letter is in guessed word the function should return True. Else the
+    function should return False.
+    >>> check('a', 'heart', 'abcdefghijklmnopqrstuvwxyz')
+    True
+    >>> check('W', 'Wonderland', 'abcdefghijklmnopqrstuvwxyz')
+    True
+    >>> check('m', 'bridge', 'abcdefghijklmnopqrstuvwxyz')
+    False
+    >>> check('e', 'economy', 'abcdijklmnopqrstuvwxyz')
+    'Oops'
+    >>> check(7, 'yellow', 'abcklmnoprsvwxyz')
+    False
+    """
+    # checking whether it is only one character and a letter
+    if isinstance(letter, str) and len(letter) == 1 and 97 <= ord(letter.lower()) <= 122:
+        # checks if letter in alphabet
+        if letter.lower() not in alphabet:
+            return "Oops"
+        #chech if letter in word
+        return letter.lower() in word.lower()
+    return False
+
+
 def check_print_words(word):
     """ Check let in word and return output
 
